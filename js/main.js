e7b2c63e568a75885640a05011df4a825607e0c8
Stephen Poole <stephen.poole@redlioncanada.com>
2015-02-26 14:57:51 -0500

    Refactoring. Made build3d object oriented.

---
+++ js/main.js
@@ -0,0 +1,96 @@
+var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent);
+
+// Three.JS/WebGL init vars
+var camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 10000 );
+var scene = new THREE.Scene();
+scene.fog = new THREE.FogExp2( 0xfffdf2, 0.18 );
+var renderer = new THREE.WebGLRenderer({antialias: true});
+var light = null;
+
+var initInterval;
+var objects = new _objects();
+var cityController = new objects.cityController();
+var animations = new objects.animations();
+
+// Render init
+renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.shadowMapEnabled = true;
+document.body.appendChild( renderer.domElement );
+
+function render() {
+	requestAnimationFrame( render );
+	TWEEN.update();
+	
+	//constrain camera position
+	if (Math.abs(camera.position.y) >= Math.abs(originY-camY2Extents)) mDOWN = false;
+	if (Math.abs(camera.position.y) <= Math.abs(originY+camY1Extents)) mUP = false;
+	if (Math.abs(camera.position.x) >= Math.abs(originX-camX1Extents)) mLEFT = false;
+	if (Math.abs(camera.position.x) <= Math.abs(originX+camX2Extents)) mRIGHT = false;
+	
+	if (!overlay) {
+		if (!xMove && mRIGHT) animations.CameraPanX(0.1);
+		if (!xMove && mLEFT) animations.CameraPanX(-0.1);
+		if (!yMove && mUP) animations.CameraPanY(0.1);
+		if (!yMove && mDOWN) animations.CameraPanY(-0.1);
+		if (mGOIN) animations.CameraZoom(0.1);
+		if (mGOOUT) animations.CameraZoom(-0.1);
+		if (xMove && (mLEFT || mRIGHT)) animations.CameraPanX((xMove/200) * -1);
+		if (yMove && (mUP || mDOWN)) animations.CameraPanY(yMove/200);
+		if (mROTUP && camera.rotation.x < 0.9) {
+			camera.rotation.x += 0.03;
+		}
+		if (mROTDOWN && camera.rotation.x > 0) {
+			camera.rotation.x -= 0.03;
+		}
+		renderer.render( scene, camera );
+	}
+}
+
+function init3D() {
+	// Objects init - plane (ground)
+	var geometry = new THREE.PlaneBufferGeometry( 100, 100 );
+	var material = new THREE.MeshPhongMaterial( {color: 0xfffdf2, side: THREE.DoubleSide} );
+	var plane = new THREE.Mesh( geometry, material );
+	scene.add( plane );
+	plane.position.z = -0.2;
+	
+	// Objects init - camera light
+	light = new THREE.PointLight(0xffffff, 1, 50);
+	light.position.set(0,0,4);
+	scene.add(light);
+	camera.position.z = camZStart;
+	
+	//delay building init until data is populated
+	initInterval = setInterval(function() {
+		if (glCards.length > 0) {
+			//center camera to grid
+			var gutterX = gridSizex-boxwidth;
+			var gutterY = gridSizey-boxheight;
+			var gridTotalWidth = maxX * (gridSizex + gutterX);
+			var gridTotalHeight = maxY * (gridSizey + gutterY);
+			camera.position.y = -(gridTotalHeight/2);
+			light.position.y = -(gridTotalHeight/2);
+			camera.position.x = -(gridTotalWidth/2);
+			light.position.x = -(gridTotalWidth/2);
+	
+			//set camera constraints
+			camMinX = -gridTotalWidth-camX1Extents;
+			camMaxX = 0+camX2Extents;
+			camMinY = -gridTotalHeight-camY1Extents;
+			camMaxY = 0+camY2Extents;
+		
+			//set camera origin
+			originX = camera.position.x;
+			originY = camera.position.y;
+	
+			//zoom camera
+			animations.CameraZoom(camZEnd, undefined, true);
+			
+			clearInterval(initInterval);
+			cityController.SpawnCity();
+		}
+	}, 500);
+	
+	// Start Rendering
+	render();
+}
---
+++ js/objects.js
@@ -0,0 +1,147 @@
+var _objects = function() {
+	var self = this;
+	
+	//Animations - Holds various animations to three.js objects via tween library
+	this.animations = function() {}
+	this.animations.prototype.CameraPan = function(toX, toY, fromX, fromY, abs) {
+		this.CameraPanX(fromX, toX, abs);
+		this.CameraPanY(fromY, toY, abs);
+	}
+	
+	this.animations.prototype.CameraPanX = function(to, from, abs) {
+		if (typeof abs === 'undefined') abs = false;
+		if (typeof from === 'undefined') from = camera.position.x;
+		if (!abs) to = from + to;
+		var t = new TWEEN.Tween( { x : from } )
+			.to( { x : to }, camZAnimationTime*1000 )
+			.onUpdate( function() {
+				camera.position.x = this.x;
+			})
+			.start();
+	}
+	
+	this.animations.prototype.CameraPanY = function(to, from, abs) {
+		if (typeof abs === 'undefined') abs = false;
+		if (typeof from === 'undefined') from = camera.position.y;
+		if (!abs) to = from + to;
+		var t = new TWEEN.Tween( { y : from } )
+			.to( { y : to }, camZAnimationTime*1000 )
+			.onUpdate( function() {
+				camera.position.y = this.y;
+			})
+			.start();
+	}
+	
+	this.animations.prototype.CameraZoom = function(to, from, abs) {
+		if (typeof abs === 'undefined') abs = false;
+		if (typeof from === 'undefined') from = camera.position.z;
+		if (!abs) to = from + to;
+		var t = new TWEEN.Tween( { z : from } )
+			.to( { z : to }, camZAnimationTime*1000 )
+			.easing( TWEEN.Easing.Elastic.InOut )
+			.onUpdate( function() {
+				camera.position.z = this.z;
+			})
+			.start();
+	}
+	//End Animations
+	
+	//CityController - Maintains cities
+	this.cityController = function() {
+		this.cities = [];
+	};
+
+	this.cityController.prototype.SpawnCity = function() {
+		var c = new self.city();
+		this.cities.push(c);
+	}
+	//End CityController
+
+	//City - A single content entity, holds it's physical data as well as it's content data
+	this.city = function() {
+		this.logMatrix = function(matrix) {
+			for (var arr in matrix) {
+				for (var index in arr) {
+					console.log(matrix[arr][index]);
+				}
+			}
+		}
+	
+		this.buildings = [];
+		this.buildingData;
+		this.init3D();
+	};
+
+	this.city.prototype.init3D = function() {
+		var gutterX = gridSizex-boxwidth;
+		var gutterY = gridSizey-boxheight;
+		var jitterxBool = jitterX;
+		var jitteryBool = jitterY;
+	
+		// Objects init - boxes (buildings)
+		this.drawMatrix = math.zeros(maxX, maxY);
+		this.dataMatrix = math.zeros(1, glCards.length);
+		camMinHeight = 0;
+	
+		this.buildingData = glCards.slice();
+		var buildingDataLength = this.buildingData.length;
+		var br = false;
+		var ind = 0;
+	
+		for (var y = 1; y <= maxY; y++) {
+			for (var x = maxX; x >= 1; x--) {
+				var thisbox = {};
+				var curBuilding = this.buildingData[ind];
+				if (typeof curBuilding === 'undefined') {br = true; break;}
+			
+				//if (x == 3) {br = true; break;}
+				//if the current draw matrix index is occupied, skip this index
+				if (this.drawMatrix.subset(math.index(y-1,maxX-x)) > 0) continue;
+			
+				//TODO skip this draw index if the current tile will extend into an occupied index, non-issue if only drawing right/down
+			
+				//set the current draw index as occupied
+				this.drawMatrix.subset(math.index(y-1,maxX-x), Math.max(curBuilding.xsize,curBuilding.ysize));
+			
+				//set the indexes the current item extends into as occupied
+				if (curBuilding.ysize > 1) for (var i = 1; i <= curBuilding.xsize; i++) {this.drawMatrix.subset(math.index(y+i-1,maxX-x),Math.max(curBuilding.xsize,curBuilding.ysize));}
+				if (curBuilding.xsize > 1) for (var i = 1; i <= curBuilding.ysize; i++) {this.drawMatrix.subset(math.index(y-1,maxX-x+i),Math.max(curBuilding.xsize,curBuilding.ysize));}
+			
+				//set the current data index as displayed/occupied
+				this.dataMatrix.subset(math.index(0, buildingDataLength-this.buildingData.length), Math.max(curBuilding.xsize,curBuilding.ysize));
+				this.buildingData.splice(ind, 1);
+			
+				jitterxBool *= -1;
+				jitteryBool *= -1;
+				var curBoxHeight = (gutterY*(curBuilding.ysize-1)) + boxheight*curBuilding.ysize;
+				var curBoxWidth = (gutterX*(curBuilding.xsize-1)) + boxwidth*curBuilding.xsize;
+				var curBoxDepth = (Math.random() * 3) + 1;
+				if (curBoxDepth / 2 + 1 > camMinHeight) camMinHeight = Math.ceil(curBoxDepth / 2 + 1);
+			
+				thisbox.geometry = new THREE.BoxGeometry( curBoxWidth, curBoxHeight, curBoxDepth );
+				var useColor = colors[Math.round(Math.random())];
+				thisbox.material = [
+					new THREE.MeshLambertMaterial( {color: useColor }),
+					new THREE.MeshLambertMaterial( {color: useColor }),
+					new THREE.MeshLambertMaterial( {color: useColor }),
+					new THREE.MeshLambertMaterial( {color: useColor }),
+					new THREE.MeshLambertMaterial( {map: THREE.ImageUtils.loadTexture(curBuilding.img)}),
+					new THREE.MeshLambertMaterial( {color: useColor })
+				];
+				thisbox.material[4].minFilter = THREE.NearestFilter;
+				thisbox.cube = new THREE.Mesh( thisbox.geometry, new THREE.MeshFaceMaterial(thisbox.material) );
+				thisbox.cube.name = buildingDataLength-this.buildingData.length;
+				scene.add( thisbox.cube );
+				this.buildings.push(thisbox.cube);
+				thisbox.cube.position.x = -x * gridSizex - (((curBuilding.xsize - 1) * gridSizex) / 2) + jitterxBool;
+				thisbox.cube.position.y = -y * gridSizey - (((curBuilding.ysize - 1) * gridSizey) / 2) + jitteryBool;
+
+				this.logMatrix(this.drawMatrix);
+				this.logMatrix(this.dataMatrix);
+				if (br) break;
+			}
+			if (br) break;
+		}
+	};
+	//End City
+};
